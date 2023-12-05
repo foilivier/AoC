5f9072b289117d2c5f193755b56a848a9b4fe865
Olivier REMAUD <olivier.remaud@soprasteria.com>
2023-12-05 10:57:50 +0100

    More comments for Day 5

--- 2023/Cal05.py
+++ 2023/Cal05.py
@@ -1,47 +1,5 @@
 import sys
 import re
-from collections import defaultdict
-
-def getDest(map, value) :
-    for (source, dest, len) in map :
-        if source <= value <= source + len - 1 :
-            return dest + value - source
-    return value
-
-def findMin(start, end, source) :
-    
-    if not source in mapNames :
-        return start
-    dest = mapNames[source]
-    mapName = source + "-to-" + dest
-    cMap = maps[mapName]
-
-    bestMin = sys.maxsize
-    current = start
-    while current <= end :
-        entryFound = False
-        nextMinStart = sys.maxsize
-        for eSource, eDest, eLen in cMap :
-            eEnd = eSource + eLen - 1
-            if eSource > current :
-                nextMinStart = min(nextMinStart, eSource)
-
-            if eSource <= current <= eEnd :
-                entryFound = True
-                if eEnd >= end :
-                    bestMin = min(bestMin, findMin(getDest(cMap, current), getDest(cMap, end), dest))
-                    current = end + 1
-                else :
-                    bestMin = min(bestMin, findMin(getDest(cMap, current), getDest(cMap, eEnd), dest))
-                    current = eEnd + 1
-                break;
-        if not entryFound :
-            bestMin = min(bestMin, findMin(getDest(cMap, current), getDest(cMap, min(end, nextMinStart - 1)), dest))
-            current = nextMinStart;
-
-    return bestMin;
-
-
 
 mapNames = {}
 maps = {}
@@ -49,9 +7,10 @@ currentMap = None
 currentSection = None
 seeds = None
 
+# read data
 with open("inputs/2023/05.txt") as file :
-    for l in file :
-        line = l.rstrip('\n')
+    for lg in file :
+        line = lg.rstrip('\n')
         if line.strip() == "" :
             continue
         
@@ -72,24 +31,61 @@ with open("inputs/2023/05.txt") as file :
             d = line.split()
             currentMap.append( (int(d[1]), int(d[0]), int(d[2])) ) # source, dest, len
 
+def getDest(map, value) :
+    for source, dest, len in map :
+        if source <= value <= source + len - 1 :
+            return dest + value - source
+    return value # unmapped => unchanged
 
 part1 = sys.maxsize
 for seed in seeds :
-    currentValue = seed
+    currentSeed = seed
     source = "seed"
     while source in mapNames :
         dest = mapNames[source]
         mapName = source + "-to-" + dest
         cMap = maps[mapName]
-        currentValue = getDest(cMap, currentValue)
+        currentSeed = getDest(cMap, currentSeed)
         source = dest
-    part1 = min(part1, currentValue)
+    part1 = min(part1, currentSeed)
 
 print(part1)
 
+def findMin(start, end, source) :
+    
+    if not source in mapNames :
+        return start # end of recursion, return smallest (which is the start)
+
+    dest = mapNames[source]
+    mapName = source + "-to-" + dest
+    cMap = maps[mapName]
+
+    bestMin = sys.maxsize
+    current = start
+    while current <= end : # cover full range from start to end
+        entryFound = False
+        nextMinStart = sys.maxsize
+        for eSource, eDest, eLen in cMap :
+            eEnd = eSource + eLen - 1
+            if eSource > current :
+                nextMinStart = min(nextMinStart, eSource) # find next range with minimum start
+
+            if eSource <= current <= eEnd :
+                entryFound = True # this part of range is mapped
+                realEnd = min(end, eEnd) # mapped till the end or not ?
+                bestMin = min(bestMin, findMin(getDest(cMap, current), getDest(cMap, realEnd), dest))
+                current = realEnd + 1
+                break;
+        if not entryFound : # not mapped => get values until next range (nextMinStart)
+            bestMin = min(bestMin, findMin(getDest(cMap, current), getDest(cMap, min(end, nextMinStart - 1)), dest))
+            current = nextMinStart;
+
+    return bestMin;
+
 part2 = sys.maxsize
-for i in range(0, len(seeds), 2) :
-    start = seeds[i]
-    len = seeds[i + 1]
-    part2 = min(part2, findMin(start, start + len - 1, "seed"))
+for idx in range(0, len(seeds), 2) :
+    start = seeds[idx]
+    lg = seeds[idx + 1]
+    end = start + lg - 1 # inclusive
+    part2 = min(part2, findMin(start, end, "seed"))
 print(part2)
